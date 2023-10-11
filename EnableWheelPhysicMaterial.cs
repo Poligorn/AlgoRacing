4a5141512669bdcfda942aa2d5dd7fb26e09d9f5
Poligorn <133873347+Poligorn@users.noreply.github.com>
2023-10-11 11:46:34 +0300

    Add files via upload

---
+++ EnableWheelPhysicMaterial.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EnableWheelPhysicMaterial : MonoBehaviour
+{
+    private WheelCollider wheel;
+    void Start()
+    {
+        wheel = GetComponent<WheelCollider>();
+    }
+
+    void FixedUpdate()
+    {
+        WheelHit hit;
+        if (wheel.GetGroundHit(out hit))
+        {
+            WheelFrictionCurve fFriction = wheel.forwardFriction;
+            fFriction.stiffness = hit.collider.material.staticFriction;
+            wheel.forwardFriction = fFriction;
+            WheelFrictionCurve sFriction = wheel.sidewaysFriction;
+            sFriction.stiffness = hit.collider.material.staticFriction;
+            wheel.sidewaysFriction = sFriction;
+        }
+    }
+}
